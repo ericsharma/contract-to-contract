9912397c8748fdc0c81209b3fc7e3a9693b8d2b4
ericsharma <sharma.e@husky.neu.edu>
2024-01-30 08:55:22 -0500

    Example with arguments

---
+++ contracts/AssetTrampoline.algo.ts
@@ -0,0 +1,108 @@
+import { Contract } from '@algorandfoundation/tealscript';
+
+// eslint-disable-next-line no-unused-vars
+class ChildContract extends Contract {
+  Asset = GlobalStateKey<Asset>();
+
+  Creator = GlobalStateKey<Account>();
+
+  // @allow.create()
+  // new(asset: Asset): Address {
+  //   this.Asset.value = asset;
+  //   return this.app.address;
+  // }
+
+  // createApplication(asset: Asset): void {
+  //   this.Asset.value = asset;
+  //   // return this.app.address;
+  // }
+
+  triggerOptIn(payTxn: InnerPayment, asset: Asset): void {
+    // this.Asset.value = asset;
+    // verifyPayTxn(payTxn, {
+    //   receiver: this.app.address,
+    // });
+
+    assert(payTxn.receiver === this.app.address);
+    sendAssetTransfer({
+      // xferAsset: this.Asset.value,
+      xferAsset: asset,
+      assetAmount: 0,
+      sender: this.app.address,
+      assetReceiver: this.app.address,
+    });
+  }
+
+  transferAsset(assetTransfer: AssetTransferTxn, sender: Account): void {
+    verifyAssetTransferTxn(assetTransfer, {
+      sender: globals.creatorAddress,
+      assetReceiver: globals.currentApplicationAddress,
+      // xferAsset: this.Asset.value,
+    });
+
+    // this.Creator.value = sender;
+  }
+}
+// eslint-disable-next-line no-unused-vars
+class AssetTrampoline extends Contract {
+  optInToAsa(payTxn: PayTxn, asset: Asset): void {
+    verifyPayTxn(payTxn, {
+      receiver: globals.currentApplicationAddress,
+    });
+    sendAssetTransfer({
+      xferAsset: asset,
+      assetAmount: 0,
+      sender: globals.currentApplicationAddress,
+      assetReceiver: globals.currentApplicationAddress,
+    });
+  }
+
+  sendAssetToChildContract(payTxn: PayTxn, assetTransfer: AssetTransferTxn): void {
+    // Verify input Transactions
+    verifyPayTxn(payTxn, {
+      receiver: globals.currentApplicationAddress,
+      sender: globals.creatorAddress,
+    });
+
+    assert(globals.currentApplicationAddress.hasAsset(assetTransfer.xferAsset));
+    verifyAssetTransferTxn(assetTransfer, {
+      sender: globals.creatorAddress,
+      assetReceiver: globals.currentApplicationAddress,
+    });
+
+    // Create child contract
+    sendMethodCall<[], void>({
+      name: 'createApplication',
+      clearStateProgram: ChildContract.clearProgram(),
+      approvalProgram: ChildContract.approvalProgram(),
+      // methodArgs: [assetTransfer.xferAsset],
+      // fee: 3000,
+    });
+
+    const childAppId = this.itxn.createdApplicationID;
+    sendPayment({
+      receiver: childAppId.address,
+      amount: 300_000,
+    });
+
+    sendMethodCall<[InnerPayment, Asset], void>({
+      name: 'triggerOptIn',
+      applicationID: childAppId,
+
+      methodArgs: [{ amount: 200_000, receiver: childAppId.address }, assetTransfer.xferAsset],
+    });
+
+    // sendMethodCall<[InnerAssetTransfer, Address], void>({
+    //   name: 'transferAsset',
+    //   applicationID: childAppId,
+    //   methodArgs: [
+    //     {
+    //       xferAsset: assetTransfer.xferAsset,
+    //       assetReceiver: childAppId.address,
+    //       assetAmount: assetTransfer.assetAmount,
+    //     },
+    //     globals.creatorAddress,
+    //   ],
+    // });
+  }
+}
---
+++ contracts/AssetTrampoline.test.ts
@@ -0,0 +1,104 @@
+import { describe, test, beforeAll, beforeEach } from '@jest/globals';
+// eslint-disable-next-line import/no-unresolved
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import * as algokit from '@algorandfoundation/algokit-utils';
+import algosdk from 'algosdk';
+import { AssetTrampolineClient } from './clients/AssetTrampolineClient';
+
+describe('Asset Trampoline', () => {
+  const fixture = algorandFixture();
+
+  let appClient: AssetTrampolineClient;
+
+  let asa: bigint;
+
+  let alice: algosdk.Account;
+
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { algod, testAccount } = fixture.context;
+
+    alice = testAccount;
+
+    appClient = new AssetTrampolineClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: 0,
+      },
+      algod
+    );
+
+    await appClient.create.createApplication({});
+
+    // Create an ASA to use in our tests
+    const asaCreateTxn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
+      suggestedParams: await algod.getTransactionParams().do(),
+      from: testAccount.addr,
+      total: 10000,
+      decimals: 0,
+      defaultFrozen: false,
+    });
+
+    const { confirmation } = await algokit.sendTransaction({ transaction: asaCreateTxn, from: testAccount }, algod);
+
+    asa = BigInt(confirmation!.assetIndex!);
+
+    await appClient.appClient.fundAppAccount(algokit.microAlgos(1_000_000));
+  });
+
+  test('optInToAsa Method', async () => {
+    const { algod } = fixture.context;
+    const { appAddress } = await appClient.appClient.getAppReference();
+
+    console.info(`*** Parent App ID**** ${appAddress}`);
+
+    const mbrOptInTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      suggestedParams: await algod.getTransactionParams().do(),
+      from: alice.addr,
+      amount: 100_000,
+      to: appAddress,
+    });
+
+    await appClient.optInToAsa([mbrOptInTxn, Number(asa)], { sendParams: { fee: algokit.microAlgos(3_000) } });
+  });
+
+  test('sendAssetToChilContract method', async () => {
+    const { appAddress } = await appClient.appClient.getAppReference();
+    const { algod } = fixture.context;
+
+    const childMbrOptInTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      suggestedParams: { ...(await algod.getTransactionParams().do()) },
+      from: alice.addr,
+      amount: 1_200_000,
+      to: appAddress,
+    });
+
+    const assetTransferTxn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+      from: alice.addr,
+      to: appAddress,
+      assetIndex: Number(asa),
+      amount: 10,
+      //   suggestedParams: { ...(await algod.getTransactionParams().do()), fee: algokit.microAlgos(8_000) },
+      suggestedParams: await algod.getTransactionParams().do(),
+    });
+
+    console.info({ alice });
+
+    await appClient.sendAssetToChildContract([childMbrOptInTxn, assetTransferTxn], {
+      sendParams: { fee: algokit.microAlgos(100_000) },
+    });
+
+    // *** Can't get the creater application to go through
+
+    // await factoryCaller.mintAndGetAsset(
+    //     {},
+    // {
+    //   sendParams: { fee: algokit.microAlgos(8_000) },
+    // }
+    //   )
+    // ).return?.valueOf()
+  });
+});
