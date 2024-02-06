94205ef76fc9b291c1bd747bb142bf4c346f28d2
ericsharma <sharma.e@husky.neu.edu>
2024-02-06 13:36:02 -0500

    Set up simple order execution test

--- contracts/AssetTrampoline.test.ts
+++ contracts/AssetTrampoline.test.ts
@@ -5,19 +5,21 @@ import * as algokit from '@algorandfoundation/algokit-utils';
 import algosdk from 'algosdk';
 import { AlgoAmount } from '@algorandfoundation/algokit-utils/types/amount';
 import { AssetTrampolineClient } from './clients/AssetTrampolineClient';
+import { ChildContractClient } from './clients/ChildContractClient';
 import { createAndSendAsset, createAsset, optIn } from '../utils';
 
 describe('Asset Trampoline', () => {
   const fixture = algorandFixture();
 
-  let appClient: AssetTrampolineClient;
+  let orderbookAppClient: AssetTrampolineClient;
 
   let sellIdx: bigint | number;
   let buyIdx: bigint | number;
 
   let alice: algosdk.Account;
   let bob: algosdk.Account;
-  let orderAddr: string;
+  let charlie: algosdk.Account;
+  let orderAppId: bigint | number;
 
   beforeEach(fixture.beforeEach);
 
@@ -28,8 +30,9 @@ describe('Asset Trampoline', () => {
     alice = testAccount;
 
     bob = await generateAccount({ initialFunds: new AlgoAmount({ algos: 10_000_000 }) });
+    charlie = await generateAccount({ initialFunds: new AlgoAmount({ algos: 10_000_000 }) });
 
-    appClient = new AssetTrampolineClient(
+    orderbookAppClient = new AssetTrampolineClient(
       {
         sender: testAccount,
         resolveBy: 'id',
@@ -38,43 +41,52 @@ describe('Asset Trampoline', () => {
       algod
     );
 
-    await appClient.create.createApplication({});
+    await orderbookAppClient.create.createApplication({});
 
     sellIdx = await createAndSendAsset({
       creator: testAccount,
       recievers: [bob],
-      total: BigInt(100000),
+      total: BigInt(100_000),
       amountToSend: BigInt(10),
       decimals: 2,
       algod,
     });
 
-    buyIdx = await createAsset({ creator: testAccount, algod });
+    // buyIdx = await createAsset({ creator: testAccount, algod });
+    buyIdx = await createAndSendAsset({
+      creator: testAccount,
+      recievers: [charlie],
+      total: BigInt(10_000),
+      amountToSend: BigInt(6),
+      decimals: 3,
+      algod,
+    });
 
     await optIn(bob, buyIdx, algod);
+    await optIn(charlie, sellIdx, algod);
 
-    await appClient.appClient.fundAppAccount(algokit.microAlgos(1_000_000));
+    await orderbookAppClient.appClient.fundAppAccount(algokit.microAlgos(1_000_000));
   });
 
-  test('optInToAsa Method', async () => {
+  test('open order', async () => {
     const { algod } = fixture.context;
-    const { appAddress } = await appClient.appClient.getAppReference();
+    const { appAddress } = await orderbookAppClient.appClient.getAppReference();
     const mbrOptInTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
       suggestedParams: await algod.getTransactionParams().do(),
-      from: alice.addr,
+      from: bob.addr,
       amount: 100_000,
       to: appAddress,
     });
 
     const childMbrOptInTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
       suggestedParams: { ...(await algod.getTransactionParams().do()) },
-      from: alice.addr,
+      from: bob.addr,
       amount: 1_000_001,
       to: appAddress,
     });
 
     const assetTransferTxn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
-      from: alice.addr,
+      from: bob.addr,
       to: appAddress,
       assetIndex: Number(sellIdx),
       amount: 10,
@@ -82,47 +94,60 @@ describe('Asset Trampoline', () => {
     });
 
     console.info(`*** Parent App ID**** ${appAddress}`);
-    const { returns } = await appClient
+    const { returns } = await orderbookAppClient
       .compose()
-      .optInToAsa([mbrOptInTxn, Number(sellIdx)], { sendParams: { fee: algokit.microAlgos(3_000) } })
+      .optInToAsa([mbrOptInTxn, Number(sellIdx)], { sender: bob, sendParams: { fee: algokit.microAlgos(3_000) } })
       // .optInToAsa([mbrOptInTxn, Number(buyIdx)], { sendParams: { fee: algokit.microAlgos(3_000) } })
 
       .openOrder([childMbrOptInTxn, assetTransferTxn, Number(buyIdx), 10, 20], {
+        sender: bob,
         sendParams: { fee: algokit.microAlgos(6_000) },
       })
       .execute();
 
-    const placeHolder: string = returns[1];
-    orderAddr = placeHolder;
+    const placeHolder: bigint | number = returns[1];
+    orderAppId = placeHolder;
 
-    console.info(orderAddr);
+    console.info(returns);
+
+    console.info(orderAppId);
+    console.info('###');
   });
 
   test('executeOrder', async () => {
     const { algod } = fixture.context;
-    const { appAddress } = await appClient.appClient.getAppReference();
-    const mbrOptInTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-      suggestedParams: await algod.getTransactionParams().do(),
-      from: bob.addr,
-      amount: 100_000,
-      to: appAddress,
-    });
+    const { appAddress } = await orderbookAppClient.appClient.getAppReference();
 
-    const childMbrOptInTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-      suggestedParams: { ...(await algod.getTransactionParams().do()) },
-      from: bob.addr,
-      amount: 1_000_001,
-      to: appAddress,
-    });
+    const orderAppClient = new ChildContractClient({ resolveBy: 'id', id: orderAppId }, algod);
+
+    const { appAddress: orderAppAddress } = await orderAppClient.appClient.getAppReference();
 
     const assetTransferBuyTxn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
-      from: bob.addr,
-      to: appAddress,
+      from: charlie.addr,
+      to: orderAppAddress,
       assetIndex: Number(buyIdx),
-      amount: 10,
+      amount: 6,
       suggestedParams: await algod.getTransactionParams().do(),
     });
+    await orderAppClient
+      .compose()
+      .triggerOptIn([Number(buyIdx)], { sender: charlie, sendParams: { fee: algokit.microAlgos(3_000) } })
+      .executeOrder([assetTransferBuyTxn, sellIdx, buyIdx, bob.addr], {
+        sender: charlie,
+        sendParams: { fee: algokit.microAlgos(6_000) },
+      })
+      .execute();
 
-    // Write the method to execute on an order
+    console.info(`
+      bob (seller): ${bob.addr}
+      charlie (buyer): ${charlie.addr}
+      buy IDX : ${buyIdx}
+      sell IDX: ${sellIdx}
+      sellQuant: 10,
+      buyQuant: 20,
+      charlie sends: 6
+      expect ${charlie.addr} to have 3 of ${sellIdx}
+      expect ${bob.addr} to have 6 if ${buyIdx}
+      `);
   });
 });
